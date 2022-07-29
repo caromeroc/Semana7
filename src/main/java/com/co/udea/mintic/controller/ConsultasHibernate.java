8493c91beae0c38b586b72852f943d745cec4f46
Carlos Romero <carlos.romero@one-click.com.co>
2022-07-28 19:53:21 -0500

    feat :: 3

--- src/main/java/com/co/udea/mintic/controller/ConsultasHibernate.java
+++ src/main/java/com/co/udea/mintic/controller/ConsultasHibernate.java
@@ -1,9 +1,78 @@
 package com.co.udea.mintic.controller;
 
+import com.co.udea.mintic.db.*;
+import com.co.udea.mintic.util.DBUtilSession;
+import java.sql.SQLException;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class ConsultasHibernate {
-    
-    
-    
-    
+
+    Transaction transaction = null;
+    Vendedores objV1 = new Vendedores(6666, "Carlos", "Romero", "Soacha");
+
+    public List<Vendedores> selectAllHibernateVendedor() {
+        List<Vendedores> vend = null;
+
+        try {
+            Session session = DBUtilSession.getSessionFactory().openSession();
+            vend = session.createQuery("from Vendedores", Vendedores.class).list();
+/*
+            for (Vendedores v : vend) {
+                System.out.println("Nombres: " + v.getNombres());
+            }
+*/
+        } catch (Exception e) {
+            transaction.rollback();
+            System.err.println("Error :: " + e.getCause());
+        }
+
+        return vend;
+
+    }
+
+    public String insertVendedor(Vendedores objV) {
+
+        String salida = "";
+        
+        try {
+            Session session = DBUtilSession.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(objV);
+            transaction.commit();
+            salida = "Insert terminado Correctamente";
+        } catch (Exception e) {
+            transaction.rollback();
+            System.err.println("Error :: " + e.getCause());
+            salida = "Insert Failure";
+        }
+        return salida;
+    }
+
+    public void deleteVendedor(Vendedores obj) {
+        try {
+            Session session = DBUtilSession.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(obj);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            System.err.println("Error :: " + e.getCause());
+        }
+    }
+    // To-do -- Delete cascada
+
+    public void deleteVehiculo(Vehiculos objVehi) {
+        try {
+            Session session = DBUtilSession.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(objVehi);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            System.err.println("Error :: " + e.getCause());
+        }
+    }
+
 }
--- src/main/java/com/co/udea/mintic/principal/Lanzadora.java
+++ src/main/java/com/co/udea/mintic/principal/Lanzadora.java
@@ -1,14 +1,26 @@
 package com.co.udea.mintic.principal;
 
 import com.co.udea.mintic.controller.Consultas;
+import com.co.udea.mintic.controller.ConsultasHibernate;
+import com.co.udea.mintic.db.Vehiculos;
+import com.co.udea.mintic.db.Vendedores;
 
 public class Lanzadora {
 
     public static void main(String[] args) {
-        Consultas objCon = new Consultas();     
+        //Consultas objCon = new Consultas();     
         //objCon.selectAllVendedor();
         //objCon.insertVendedor();
-        objCon.deleteVendedor();
+        //objCon.deleteVendedor();
+        ConsultasHibernate objCH = new ConsultasHibernate();
+        Vendedores objV2 =  new Vendedores(1964, "aaa", "aaa", "aaa");
+        Vehiculos objVehi = new Vehiculos("ACD 427", "", "", 0);
         
+        //objCH.insertVendedor();
+        
+        objCH.deleteVendedor(objV2);
+        objCH.deleteVehiculo(objVehi);
+        
+        objCH.selectAllHibernateVendedor();
     }  
 }
